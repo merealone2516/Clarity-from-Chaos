9860eea3fb0b631b00e87fc6a8e77aea06c5ed47
Abhishek Kumar <merealone2516@gmail.com>
2024-05-14 17:48:08 +0530

    Add files via upload

---
+++ Evaluation Metrics/BART/BARTScore.py
@@ -0,0 +1,8 @@
+
+import sys
+sys.path.append('/content/BARTScore') # Replace 'repo' with the name of the cloned repository
+from bart_score import BARTScorer
+bart_scorer.score([''' Original Text '''],
+    [''' Generated Text'''])
+
+
